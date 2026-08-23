05451402cfc3c4579f1952596f04e206a6b6ee0a
Siuo-Player <player1rgb@gmail.com>
2026-08-23 02:42:19 +0100

    fix(cython): attach compiler flags to extension

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # setup.py
 import sys
 
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 if sys.platform == "win32":
@@ -9,10 +9,18 @@ if sys.platform == "win32":
 else:
     extra_compile_args = ["-O3", "-march=native", "-mtune=native"]
 
+extensions = [
+    Extension(
+        "ai.evaluator",
+        ["ai/evaluator.pyx"],
+        extra_compile_args=extra_compile_args,
+    )
+]
+
 setup(
     name="RedWar AI Evaluator C++",
     ext_modules=cythonize(
-        "ai/evaluator.pyx",
+        extensions,
         compiler_directives={
             "language_level": "3",
             "boundscheck": False,
@@ -21,5 +29,4 @@ setup(
             "cdivision": True,
         },
     ),
-    extra_compile_args=extra_compile_args,
 )
