d0e5b9e0da1bb01db73015c5188fdfa309be6393
Rafael Bernardo <player1rgb@gmail.com>
2026-09-03 18:06:30 +0100

    test(a0): use canonical RWEN in TT fixture

--- tests/test_a0_c1_tt_observability.py
+++ tests/test_a0_c1_tt_observability.py
@@ -11,14 +11,14 @@ BRIDGE = ROOT / BRIDGE_NAME
 # Exact non-terminal fixture from the historical A/A-B failure. Its native
 # root move generation is independently known to produce four legal Lich moves.
 FIXTURE = (
-    ".,.,.,B:Obelisk_0_N_0,.,B:Cleric_0_N_0,.,B:BoneLord_0_N_0/"
-    ".,.,.,B:Ranger_0_N_0,.,.,.,./"
-    ".,.,.,.,.,.,.,./"
-    ".,.,.,.,.,.,.,./"
-    ".,.,.,.,.,.,.,./"
-    ".,.,.,B:Inquisitor_0_N_0,.,W:Obelisk_0_N_0,.,./"
-    ".,.,.,.,B:Nightshade_0_N_0,W:Lich_0_N_0,.,./"
-    ".,.,.,.,.,.,.,. W 0"
+    ".:.,.:.,.:.,B_Obelisk_0_N_0:.,.:.,B_Cleric_0_N_0:.,.:.,B_BoneLord_0_N_0:./"
+    ".:.,.:.,.:.,B_Ranger_0_N_0:.,.:.,.:.,.:./"
+    ".:.,.:.,.:.,.:.,.:.,.:.,.:.,.:./"
+    ".:.,.:.,.:.,.:.,.:.,.:.,.:.,.:./"
+    ".:.,.:.,.:.,.:.,.:.,.:.,.:.,.:./"
+    ".:.,.:.,B_Inquisitor_0_N_0:.,.:.,W_Obelisk_0_N_0:.,.:.,.:./"
+    ".:.,.:.,.:.,B_Nightshade_0_N_0:.,W_Lich_0_N_0:.,.:./"
+    ".:.,.:.,.:.,.:.,.:.,.:.,.:.,.:. W 0"
 )
 
 
